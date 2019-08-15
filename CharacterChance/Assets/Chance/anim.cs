3fcfc094c9f7d4b72734524ffec9c02f69832870
Modestas Valauskas <valauskasmodestas@gmail.com>
2019-08-15 23:11:26 +0200

    Initial commit

---
+++ CharacterChance/Assets/Chance/anim.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class anim : MonoBehaviour {
+
+    private Animator animator;
+
+	void Start () {
+        animator = GetComponent<Animator>();
+    }
+
+    float speed = 1.0f;
+    float jumpSpeed = 8f;
+    Vector3 move;
+    void FixedUpdate() {
+
+        
+
+
+
+        if (Input.GetKey("right"))
+        {
+            animator.SetBool("jumping", false);
+            Vector3 theScale = transform.localScale;
+            theScale.x = 1;
+            transform.localScale = theScale;
+
+            move = new Vector3(30, 0, 0);
+
+
+            animator.SetFloat("speed", 1f);
+        }
+        else if (Input.GetKey("left"))
+        {
+            animator.SetBool("jumping", false);
+            Vector3 theScale = transform.localScale;
+            theScale.x = -1;
+            transform.localScale = theScale;
+
+
+            move = new Vector3(-30, 0, 0);
+
+            animator.SetFloat("speed", 1f);
+        }
+        else if (Input.GetKey("space"))
+        {
+            animator.SetBool("jumping", true);
+        }
+
+        
+        transform.position += move * speed * Time.deltaTime;
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # unity_character_chance
+
+(2015)
+Some sprites drawn in school turned into a game character called Chance.
